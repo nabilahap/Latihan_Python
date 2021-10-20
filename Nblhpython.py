6db6e1282721ebb7c59070114f229a84bb55e70f
nabilahap <nabilaananda1325@gmail.com>
2021-10-20 18:25:04 +0700

    File Python

--- Nblhpython.py
+++ Nblhpython.py
@@ -1,52 +1,18 @@
-# Latihan_Python
-
-## Instalasi Python
-Download Python terlebih dahulu, di website resmi [Python](https://www.python.org/)
-
-# Cara Input Menggunakan Python
-- Buka IDLE Python di pencarian
-
-![Gambar 1](ss python/py1.png)
-
-- Kemudian klik 
-```bash
-> File
-> New File
-```
-
-![Gambar 2](ss python/py2.png)
-
-- Setelah itu klik
-```bash
-> File
-> Save
-```
-
-![Gambar 3](ss python/py3.png)
-
-- Untuk menyimpan file Python
-```bash
-> "JANGAN LUPA MENGGUNAKAN .py"
-```
-
-![Gambar 4](ss python/py4.png)
-
-- Masukan koding yang kalian ingin gunakan
-
-![Gambar 5](ss python/py4.png)
-
-- Kemudian klik Run untuk menjalankan program
-```bash
-> Run
-> Run Module
-```
-
-![Gambar 6](ss python/py6.png)
-
-## Output
-
-![Gambar 7](ss python/py7.png)
-
-- Selesai
-
-## Terima Kasih
+print ("Tugas Python")
+print ()
+nama = input("Masukan Nama Anda :")
+pgln = input("Masukan Panggilan :")
+nim = input ("Masukan NIM :")
+tglhr = input ("Masukan Tanggal Lahir :")
+tmlhr = input ("Masukan Tempat Lahir :")
+tlpn = input ("Masukan Telepon :")
+almt = input ("Masukan Alamat :")
+umur = input ("Masukan Umur Anda :")
+print ()
+print ("Assalamua'laikum")
+print ()
+print ("Let me introduce my self. my name is",nama,"but you can call me",pgln,
+        "My NIM is",nim,"I was born in",tmlhr,"and I am",umur,"years old. I am very gald if you want to invite my house in",almt,
+       ". So, don't forget to call me before with the number",tlpn,)
+print ()
+print ("THANK YOU")
--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Latihan_Python
+
+## Instalasi Python
+Download Python terlebih dahulu, di website resmi [Python](https://www.python.org/)
+
+# Cara Input Menggunakan Python
+- Buka IDLE Python di pencarian
+
+![Gambar 1](ss python/py1.png)
+
+- Kemudian klik 
+```bash
+> File
+> New File
+```
+
+![Gambar 2](ss python/py2.png)
+
+- Setelah itu klik
+```bash
+> File
+> Save
+```
+
+![Gambar 3](ss python/py3.png)
+
+- Untuk menyimpan file Python
+```bash
+> "JANGAN LUPA MENGGUNAKAN .py"
+```
+
+![Gambar 4](ss python/py4.png)
+
+- Masukan koding yang kalian ingin gunakan
+
+![Gambar 5](ss python/py4.png)
+
+- Kemudian klik Run untuk menjalankan program
+```bash
+> Run
+> Run Module
+```
+
+![Gambar 6](ss python/py6.png)
+
+## Output
+
+![Gambar 7](ss python/py7.png)
+
+- Selesai
+
+## Terima Kasih
